fd7104c5195e06787d5a046233f93fb2265898f8
faiq fananie <faiqzigofananie.95@gmail.com>
2023-01-11 14:26:48 +0700

    Add Like Domain

---
+++ src/Domains/likes/LikeRepository.js
@@ -0,0 +1,19 @@
+class LikeRepository {
+  async addLike() {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteLike() {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getLike() {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getLikes() {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = LikeRepository;
